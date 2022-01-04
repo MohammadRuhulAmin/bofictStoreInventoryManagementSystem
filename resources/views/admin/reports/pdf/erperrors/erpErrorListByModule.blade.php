ec69c6c070c6740b9ac5c1f34fe43e0f8426940e
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-01-04 07:44:04 +0600

    finally done of erp report

--- resources/views/admin/reports/pdf/erperrors/erpErrorListByModule.blade.php
+++ resources/views/admin/reports/pdf/erperrors/erpErrorListByModule.blade.php
@@ -6,15 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bangladesh Ordnance Factories</title>
     <style>
-        table, th, td {
+        table, th, td{
           border: 1px solid black;
           border-collapse: collapse;
           text-align: justify;
             text-justify: inter-word;
+            font-family: :'Times New Roman', Times, serif;
+            
+        }
+        a{
+            text-align:justify;
+            text-justify: inter-word;
         }
         </style>
 </head>
-
 <body>
         <div style="text-align:center">
             <h3>{{$Title }}  ({{$Dept}})</h3>
@@ -33,49 +38,49 @@
         </table>
         <br>
         @for ($i = 0;$i<$totalError;$i++)
-        <table  style="border-spacing: 0;" border="1px">
+        <table  style="border-spacing: 0; width:100%"  border="1px">
             <tr>
-                <td>Index</td><td>{{$i}}</td>
+                <td style="width: 30%">Index</td><td>{{$i+1}}</td>
             </tr>
             <tr>
                 <td style="width: 30%">Status</td>
-                <td>{{$erpProblem[$i]->status}}</td>
+                <td >{{$erpProblem[$i]->status}}</td>
             </tr>
             <tr>
-                <td>Module</td>
-                <td>{{$erpProblem[$i]->module}}</td>
+                <td style="width: 30%">Module</td>
+                <td >{{$erpProblem[$i]->module}}</td>
             </tr>
             <tr>
-                <td>Problem Link /  Form Link   </td>
-                <td><a href="{{$erpProblem[$i]->form_link}}">{{$erpProblem[$i]->form_link}}</a></td>
+                <td style="width: 30%">Problem Link   </td>
+                <td > <small><a href="{{$erpProblem[$i]->form_link}}">{{$erpProblem[$i]->form_link}}</a></small></td>
             </tr>
             <tr>
-                <td>Problem Description / Suggation Description </td>
-                <td>{{$erpProblem[$i]->problemDescription}}</td>
+                <td style="width: 30%">Problem Description  </td>
+                <td >{{$erpProblem[$i]->problemDescription}}</td>
             </tr>
             <tr>
-                <td>Problem Found  Date </td>
-                <td>{{$erpProblem[$i]->problemFindingDate}}</td>
+                <td style="width: 30%">Problem Found  Date </td>
+                <td >{{$erpProblem[$i]->problemFindingDate}}</td>
+            </tr>
+
+            <tr>
+                <td style="width: 30%">Problem Solution Date </td>
+                <td >{{$erpProblem[$i]->problemSolutionDate}}</td>
             </tr>
             <tr>
-                <td>Problem Solution Date </td>
-                <td>{{$erpProblem[$i]->problemSolutionDate}}</td>
+                <td style="width: 30%">Problem Detected  By </td>
+                <td >{{$erpProblem[$i]->problem_detected_by}}</td>
             </tr>
             <tr>
-                <td>Problem Detected  By </td>
-                <td>{{$erpProblem[$i]->problem_detected_by}}</td>
+                <td style="width: 30%">Remarks</td>
+                <td >{{$erpProblem[$i]->remarks}}</td>
             </tr>
             <tr>
-                <td>Remarks</td>
-                <td>{{$erpProblem[$i]->remarks}}</td>
-            </tr>
-            <tr>
-                <td>Suggation</td>
+                <td style="width: 30%">Suggation</td>
                 <td>{{$erpProblem[$i]->suggation}}</td>
             </tr>
         </table>
         <br><br>
         @endfor
-        
 </body>
 </html>
